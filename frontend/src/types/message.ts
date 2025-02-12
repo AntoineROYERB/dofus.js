7ff2b2d965cf1a6d5d84cda4df1f1612f0aae94e
Antoine Royer--Bournelle <royerb.antoine@gmail.com>
2025-02-12 21:03:46 +0100

    refactor: update message types

--- frontend/src/types/message.ts
+++ frontend/src/types/message.ts
@@ -1,41 +1,44 @@
-import { GameAction, Player } from './game';
-
+import { GameAction, Player } from "./game";
 
 export type UserInfo = {
-    id: string;
-    name: string;
-}
+  id: string;
+  name: string;
+};
 
 export type BaseMessage = {
-    messageId: string;
-    timestamp: number;
-    user: UserInfo;
-    type: MessageType;
-    gameStatus: gameStatus;
-    
-}
+  messageId: string;
+  timestamp: number;
+  userId: string;
+  userName: string;
+  type: MessageType;
+};
 export interface UserInitMessage extends BaseMessage {
-    type: 'user_init';
-    gameStatus: 'create_character';
-    }
- 
+  type: "user_init";
+  gameStatus: "create_character";
+}
+
 export interface ChatMessage extends BaseMessage {
-    type: 'chat';
-    content: string;
+  type: "chat";
+  content: string;
 }
 
 export interface PlayerMessage extends Player {
-    status: string,
+  status: string;
 }
 
 export interface GameStateMessage extends BaseMessage {
-    type: 'game_state';
-    turnNumber: number;
-    state: GameAction[];
-    players: { [key: string]: PlayerMessage };
+  type: "game_state";
+  turnNumber: number;
+  state: GameAction[];
+  gameStatus: gameStatus;
+
+  players: { [key: string]: PlayerMessage };
 }
 
+export type MessageType = "chat" | "game_action" | "game_state" | "user_init";
 
-
-export type MessageType = 'chat' | 'game_action' | 'game_state' | 'user_init';
-export type gameStatus = 'create_character' | 'start_game' | 'end_turn' | 'move';
+export type gameStatus =
+  | "create_character"
+  | "start_game"
+  | "end_turn"
+  | "move";
