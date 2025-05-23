2b1636a2882ba8c3d61604cb891682c07d1351b2
Antoine Royer--Bournelle <royerb.antoine@gmail.com>
2025-05-23 18:27:21 +0200

    feat: add PLAYER_COLORS constant for player color options

---
+++ frontend/src/constants.ts
@@ -0,0 +1,11 @@
+// Available colors for players
+export const PLAYER_COLORS = [
+  "#ff0000", // red
+  "#0000ff", // blue
+  "#008000", // green
+  "#800080", // purple
+  "#ffa500", // orange
+  "#ffc0cb", // pink
+  "#008080", // teal
+  "#4b0082", // indigo
+];
